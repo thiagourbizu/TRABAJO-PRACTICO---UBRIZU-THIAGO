aad199403e4397a2f56545a33bbffa3ea42432e0
thiagourbizu <thiago.urbizu@gmail.com>
2026-06-29 10:22:38 -0300

    validaciones de ingreso en base al campo activo

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -1,14 +1,14 @@
 <?php
-// 1. Retomamos la sesión actual
+// Retomamos la sesión actual
 session_start();
 
-// 2. Vaciamos todas las variables de sesión
+// Vaciamos todas las variables
 session_unset();
 
-// 3. Destruimos la sesión en el servidor
+// Destruimos
 session_destroy();
 
-// 4. Redirigimos al usuario al login
+// Redirigimos al usuario al login
 header("Location: ingreso.html");
 exit();
 ?>
--- ingreso.php
+++ ingreso.php
@@ -7,13 +7,14 @@ $host = 'localhost';
 $db   = 'mi_banco_db';
 $user = 'root'; 
 $pass = 'root'; 
-$port = 3307; // 3307 porque tengo otra base en 3306
+$port = 3306; // 3307 porque tengo otra base en 3306
 
-// Instanciamos la conexion
+// Instanciamos la conn
 $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("Error crítico de conexión: " . $conn->connect_error);
 }
+
 
 // capturamos la info del form de ingreso.html
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -23,18 +24,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['usuario'];
     $password = $_POST['password'];
     
-    $sql = "SELECT nombre, apellido FROM usuarios 
+    // AGREGAMOS ACTIVO AL SELECT
+    $sql = "SELECT nombre, apellido, activo FROM usuarios 
             WHERE tipo_doc = ? AND documento = ? AND usuario = ? AND password = ?";
             
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ssss", $tipo_doc, $documento, $usuario, $password);
     $stmt->execute();
+    
     // resultado de la consulta
     $resultado = $stmt->get_result();
     
     // validamos sesion
     if ($resultado->num_rows > 0) {
         $fila = $resultado->fetch_assoc();
+        
+        // validamos si es activo.
+        if ($fila['activo'] == 0) {
+            echo "<h3>Cuenta Inactiva</h3>";
+            echo "<p>Su cuenta fue desactivada por el banco. No puede acceder al sistema web.</p>";
+            echo "<a href='ingreso.html'>Volver al Login</a>";
+            exit();
+        }
+        
         // variables para durante la SESION
         $_SESSION['logueado'] = true;
         $_SESSION['documento'] = $documento;
@@ -44,7 +56,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: resumen.php");
         exit();
     } else {
-        // Credenciales inválidas
         echo "<h3>Acceso Denegado</h3>";
         echo "<p>Los datos ingresados son incorrectos. Por favor, verifica tu información.</p>";
         echo "<a href='ingreso.html'>Volver al Login</a>";
